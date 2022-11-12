dab6d8bed5bd1461c94a9a650e1e8ef9ba600bb5
Amil Silahic <jinobie02@gmail.com>
2022-11-12 10:54:49 -0700

    adding infoCard component

---
+++ components/InfoCard/InfoCard.js
@@ -0,0 +1,16 @@
+const InfoCard = (props) => {
+  return (
+    <div className="flex gap-4 bg-white h-32 items-center justify-center">
+      <h1>IP HERE</h1>
+      {/* vertical bar */}
+      <div className="divider divider-horizontal"></div>
+      <h1>Location HERE</h1>
+      <div className="divider divider-horizontal"></div>
+      <h1>TimeZone HERE</h1>
+      <div className="divider divider-horizontal"></div>
+      <h1>ISP HERE</h1>
+    </div>
+  );
+};
+
+export default InfoCard;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import FormInput from "../components/FormInput/FormInput";
+import InfoCard from "../components/InfoCard/InfoCard";
 
 const HomePage = () => {
   return (
     <>
       <h1 className="text-2xl bg-red-500 font-mono">IP Address Tracker</h1>
       <FormInput />
+      <InfoCard />
     </>
   );
 };
