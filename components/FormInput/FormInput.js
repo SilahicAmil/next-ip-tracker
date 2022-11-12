a646d3ace57f1397432d6624cfbb1d6743521d0a
Amil Silahic <jinobie02@gmail.com>
2022-11-12 11:19:08 -0700

    added getIpInfo Logic

--- components/FormInput/FormInput.js
+++ components/FormInput/FormInput.js
@@ -3,26 +3,38 @@ import { useRef } from "react";
 const FormInput = (props) => {
   const ipAddressRef = useRef();
 
-  const formSubmitHandler = (event) => {
+  const formSubmitHandler = async (event) => {
     event.preventDefault();
 
     const ipAddressRefValue = ipAddressRef.current.value;
-    console.log(ipAddressRefValue);
+
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_jLdWqCECrDZ5WNMDQd7RSSkc4HHTF&ipAddress=${ipAddressRefValue}`,
+      {
+        method: "GET",
+      }
+    );
+    const data = response.json();
+
+    console.log(data);
+
     ipAddressRef.current.value = "";
   };
   return (
-    <form action="">
-      <label htmlFor="Input IP Address" />
-      <input
-        type="text"
-        ref={ipAddressRef}
-        placeholder="Enter IP Address Here"
-        className="bg-white text-black"
-      />
-      <button type="submit" className="btn" onClick={formSubmitHandler}>
-        Submit
-      </button>
-    </form>
+    <>
+      <form action="">
+        <label htmlFor="Input IP Address" />
+        <input
+          type="text"
+          ref={ipAddressRef}
+          placeholder="Enter IP Address Here"
+          className="bg-white text-black"
+        />
+        <button type="submit" className="btn" onClick={formSubmitHandler}>
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
---
+++ pages/api/getIpInfo.js
@@ -0,0 +1,8 @@
+// const handler = async (req, res) => {
+//   if (req.method === "GET") {
+//     const ipData = req.body;
+
+//   }
+// };
+
+// export default handler;
