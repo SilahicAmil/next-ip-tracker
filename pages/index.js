a921e7f924ece45e6e9083c493c04dbf470ff8aa
Amil Silahic <jinobie02@gmail.com>
2022-11-12 10:45:44 -0700

    adding inputref logic

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
+import { use, useRef } from "react";
+
 const HomePage = () => {
-  return <h1 className="text-2xl bg-red-500">IP Address Tracker</h1>;
+  const ipAddressRef = useRef();
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const ipAddressRefValue = ipAddressRef.current.value;
+    console.log(ipAddressRefValue);
+    ipAddressRef.current.value = "";
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl bg-red-500 font-mono">IP Address Tracker</h1>
+      <form action="">
+        <label htmlFor="Input IP Address" />
+        <input
+          type="text"
+          ref={ipAddressRef}
+          placeholder="Enter IP Address Here"
+          className="bg-white text-black"
+        />
+        <button type="submit" className="btn" onClick={formSubmitHandler}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default HomePage;
